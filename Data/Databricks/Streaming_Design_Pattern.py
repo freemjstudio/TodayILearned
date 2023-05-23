97fb84f70d353983e069ea7de551715df3aa3845
freemjstudio <mjwoo0@naver.com>
2023-05-23 13:50:30 +0900

    aggregation

--- Data/Databricks/Streaming_Design_Pattern.py
+++ Data/Databricks/Streaming_Design_Pattern.py
@@ -11,4 +11,20 @@ def update_silver():
     
     query.awaitTermination()
 
-update_silver()
+# update Key-Value Aggregates (ex. sum, mean, count ...)
+
+def update_key_value():
+    query = (spark.readStream # read the streaming data 
+                  .table("silver")
+                  .groupBy("id")
+                  .agg(F.sum("value").alias("total_value"), # aggregate data 
+                       F.mean("value").alias("avg_value"),
+                       F.count("value").alias("record_count"))
+                  .writeStream # write the stream to the new table 
+                  .option("checkpointLocation", f"{DA.paths.checkpoints}/key_value")
+                  .outputMode("complete")
+                  .trigger(availableNow=True)
+                  .table("key_value"))
+    
+    query.awaitTermination()
+    
