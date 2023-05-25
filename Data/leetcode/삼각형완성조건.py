4b0cbae14a25404926f17fbbe767ed91e90dfcc0
freemjstudio <mjwoo0@naver.com>
2023-05-25 11:02:07 +0900

    삼각형 완성조건

--- Data/leetcode/삼각형완성조건.py
+++ Data/leetcode/삼각형완성조건.py
@@ -4,13 +4,12 @@ def solution(sides):
     # 가장 긴변인 경우 
     m = max(sides)
     n = min(sides)
-    # m > x + n 
-    for i in range(n+1, m-n):
+    # m < x + n 
+    for i in range(m-n+1, m):
         answer.add(i)
     
     # 나머지 한변이 가장 긴 변인 경우 
     # x < m + n 
     for j in range(m, m+n):
         answer.add(j)
-    
     return len(answer)
