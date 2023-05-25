d9cede40b8c528c8a9b926b3af19c1f4168f18f2
freemjstudio <mjwoo0@naver.com>
2023-05-25 10:57:53 +0900

    [Pro]

---
+++ Data/leetcode/삼각형완성조건.py
@@ -0,0 +1,16 @@
+def solution(sides):
+    answer = set()
+    
+    # 가장 긴변인 경우 
+    m = max(sides)
+    n = min(sides)
+    # m > x + n 
+    for i in range(n+1, m-n):
+        answer.add(i)
+    
+    # 나머지 한변이 가장 긴 변인 경우 
+    # x < m + n 
+    for j in range(m, m+n):
+        answer.add(j)
+    
+    return len(answer)
