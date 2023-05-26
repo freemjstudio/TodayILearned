6c72d2fd3464c1aba769aa6f40cb8bb41e65d127
freemjstudio <mjwoo0@naver.com>
2023-05-26 14:52:42 +0900

    import file

--- Data/Spotify/spotify_api.py
+++ Data/Spotify/spotify_api.py
@@ -1,12 +1,13 @@
 # Get Data from Spotify 
 # Without user authentication 
+from test import * 
 
 import spotipy 
 from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="",
-                                                           client_secret=""))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                           client_secret=CLIENT_SECRET))
 
-results = sp.search(q='weezer', limit=20)
+results = sp.search(q='Oasis', limit=20)
 for idx, track in enumerate(results['track']['items']):
     print(idx, track['name'])
